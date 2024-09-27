c46a267802e6d5c1db8ceb92da74050ef782fcfc
SAJADMANAVI <sajadmanavi82@gmail.com>
2024-09-27 20:59:34 +0330

    start branch

--- js/products.js
+++ js/products.js
@@ -1,46 +1,35 @@
-// let productsMain = document.querySelector(".btn-1");
-// let showMenu = "";
-// let fetchProductsMain = () => {
-//   axios
-//     .get("http://localhost:3000/Products")
-//     .then((res) => {
-//       res.data.forEach((element) => {
-//         showMenu += `
-//         <div class="item">
-//         <div>
-//           <img src="${element.image}" alt""/>
-//           <p>${element.p}</p>
-//           <h2>${element.price}</h2>
-//         </div>
-//         </div>
-//             `;
-//       });
-//       document.getElementById("root").innerHTML = showMenu;
-//     })
-//     .catch((err) => console.log("err"));
-// };
-
-// productsMain.addEventListener("DOMContentLoaded", fetchProductsMain);
-
-// CLASSES START
-let productsMain = async () => {
+let productsMain = async (url) => {
   let showMenu = "";
-  let res = await fetch("http://localhost:3000/Products");
+  let res = await fetch(url);
   let data = await res.json();
 
   data.forEach((element) => {
     showMenu += `
-    <div class="item">
-    <div class="cursor">
-    <img src="${element.image}" class="hover" alt""/>
-    <p class="font-main">${element.p}</p>
-    <h2 class="p font-main">${element.price}</h2>
-    </div>
-    </div>
+      <div class="item">
+        <div class="cursor">
+          <img src="${element.image}" class="hover" alt=""/>
+          <p class="font-main">${element.p}</p>
+          <h2 class="p font-main">${element.price}</h2>
+        </div>
+      </div>
     `;
   });
   document.querySelector("div#root").innerHTML = showMenu;
 };
 
-document.addEventListener("DOMContentLoaded", productsMain);
-// CLASSES END
+
+document.addEventListener("DOMContentLoaded", () => productsMain("http://localhost:3000/Products"));
+
+
+document.getElementById("btn-1").addEventListener("click", () => {
+  productsMain("http://localhost:3000/Products");
+});
+
+document.getElementById("btn-2").addEventListener("click", () => {
+  productsMain("http://localhost:3000/ProductsPage2");
+});
+
+document.getElementById("btn-3").addEventListener("click", () => {
+  productsMain("http://localhost:3000/ProductsPage3");
+});
+
